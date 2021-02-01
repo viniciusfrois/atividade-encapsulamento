d3545c08ae2421c0d3c70ce6615405e0a9b2f9c9
Vinicius Frois <76525389+viniciusfrois@users.noreply.github.com>
2021-02-01 14:56:49 -0300

    Add files via upload

---
+++ src/aplication/ProgramaBanco.java
@@ -0,0 +1,51 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.ContaBancaria;
+
+public class ProgramaBanco {
+
+	public static void main(String[] args) {
+		//Locale,Scanner,Variavel
+		Locale.setDefault(Locale.US);
+		Scanner teclado = new Scanner (System.in);
+		double depositodaConta = 0;
+		
+		//Entradas
+		System.out.println("Informe Numero da Conta");		
+		int numerodaConta = teclado.nextInt();
+		
+		System.out.println("Informe Nome da Conta");
+		teclado.nextLine();
+		String nomedaConta = teclado.nextLine();
+		
+		System.out.println("Deseja fazer um deposito Inicial? (S/N)");
+		char resposta = teclado.next().charAt(0);
+		
+		
+		//Verifica Condição da respota
+		if (resposta == 'S' || resposta == 's') {
+			System.out.println("Qual valor do Deposito?");
+			depositodaConta = teclado.nextDouble();
+		} else {
+		
+		}
+		//Instanciando Objeto ContaBancaria + Construtor
+		ContaBancaria contabancaria = new ContaBancaria(numerodaConta, nomedaConta, depositodaConta);
+		System.out.println(contabancaria);
+		//Chamando método adicionaSaldo
+		System.out.println("Informe outro valor de Deposito:");
+		double valor2 = teclado.nextDouble();
+		contabancaria.adicionaSaldo(valor2);
+		System.out.println("Saldo Atualizado da " + contabancaria);	
+		//Chamando método removeSaldo
+		System.out.println("Informe um valor de Saque:");
+		double valor3 = teclado.nextDouble();
+		contabancaria.removeSaldo(valor3);
+		System.out.println("Saldo Atualizado da " + contabancaria);	
+	
+		teclado.close();		
+	}
+
+}
---
+++ src/entities/ContaBancaria.java
@@ -0,0 +1,51 @@
+package entities;
+
+public class ContaBancaria {
+	//Atributos Privados
+	private int numeroConta;
+	private String nomeConta;
+	private double depositoConta;
+	
+	//Contrutor
+	public ContaBancaria(int numeroConta, String nomeConta, double depositoConta) {
+		this.numeroConta = numeroConta;
+		this.nomeConta = nomeConta;
+		adicionaSaldo(depositoConta);
+	}
+	//Getter e Setter para Acessar ou Modificar os Atributos Privados *NumeroConta e Deposito só GET
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public String getNomeConta() {
+		return nomeConta;
+	}
+
+	public void setNomeConta(String nomeConta) {
+		this.nomeConta = nomeConta;
+	}
+
+	public double getDepositoConta() {
+		return depositoConta;
+	}
+	
+	//Metodos Calculos
+	public void adicionaSaldo (double valor) {
+		this.depositoConta += valor;		
+	}
+	public void removeSaldo (double valor) {
+		this.depositoConta -= valor + 5.00;
+	}
+	
+	//Metodo imprimir OBJETO 
+	@Override
+	public String toString() {
+		return    String.format("Conta Bancaria:%nNumero da Conta = %d, ", numeroConta)				 
+				+ String.format("Nome da Conta = %s, ", nomeConta)   
+				+ String.format("Deposito da Conta = %.2f", depositoConta);
+				
+	}
+	
+	
+	
+}
